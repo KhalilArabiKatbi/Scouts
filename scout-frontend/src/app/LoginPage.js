97faff4ecf0272ac15e8d9b767879181d9021a7f
KhalilArabiKatbi <hydragamingnew@gmail.com>
2025-07-17 18:11:43 +0300

    Revert "Feat: Add hero to login and home button to navbar"
    This reverts commit 75c9d79424831c0106744b3ec2209a032390cd28.

--- scout-frontend/src/app/LoginPage.js
+++ scout-frontend/src/app/LoginPage.js
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation'; // For App Router navigation
 import Header from './Header';
-import Hero from './Hero';
 
 const API_BASE_URL = 'http://localhost:8000/api'; // Base API URL
 
@@ -62,7 +61,6 @@ export default function LoginPage() {
     <div className="relative min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/hero-1.jpg')" }}>
       <div className="absolute inset-0 bg-black bg-opacity-60" />
       <Header />
-      <Hero />
       <div className="relative flex items-center justify-center min-h-screen">
         <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-xl shadow-2xl">
           <h2 className="text-3xl font-bold text-center text-[rgba(90,25,225,0.84)]">Login</h2>

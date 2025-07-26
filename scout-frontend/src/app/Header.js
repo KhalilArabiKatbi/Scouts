e50f37702e5ee693d03a46482e618f10f715d5b5
KhalilArabiKatbi <hydragamingnew@gmail.com>
2025-07-26 16:31:38 +0300

    changed header color

--- scout-frontend/src/app/Header.js
+++ scout-frontend/src/app/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
 
   return (
     <>
-      <header className={`absolute top-0 left-0 right-0 z-10 ${isTransparent ? 'bg-transparent' : 'bg-black'}`}>
+      <header className={`absolute top-0 left-0 right-0 z-10 ${isTransparent ? 'bg-transparent' : 'bg-[rgba(90,25,225,0.84)]'}`}>
         <div className="container mx-auto px-6 py-4 flex justify-between items-center">
           <div className="flex items-center">
             <Image src="/s.svg" alt="App Logo" width={150} height={50} />
